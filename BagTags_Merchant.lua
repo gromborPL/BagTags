a686ac4e209faec01efcb505e801ff6a04cec9f7
gromborPL <gromgaming@yahoo.com>
2026-08-03 08:42:28 +0200

    Refactor vendor item functions to use BT methods

--- BagTags_Merchant.lua
+++ BagTags_Merchant.lua
@@ -58,98 +58,11 @@ sellButton:SetPoint("BOTTOM", 0, 20)
 sellButton:SetText("Sell Vendor Items")
 
 --------------------------------------------------
--- Skany
---------------------------------------------------
-
-function Merchant:GetVendorItems()
-	local data = {
-		items = {},
-		count = 0,
-		value = 0,
-	}
-
-	if BT.BuildCurrentItemCounts then
-		BT:BuildCurrentItemCounts()
-	end
-
-	for bag = 0, 4 do
-		local numSlots = BT:GetContainerNumSlots(bag)
-
-		for slot = 1, numSlots do
-			local link = BT:GetContainerItemLink(bag, slot)
-
-			if link then
-				local _, _, _, _, _, _, _, _, _, _, vendorPrice = GetItemInfo(link)
-
-				vendorPrice = vendorPrice or 0
-
-				if vendorPrice > 0 and BT:GetValueTag(bag, slot) == "V" then
-					local _, stackCount = BT:GetContainerItemInfo(bag, slot)
-
-					stackCount = stackCount or 1
-
-					table.insert(data.items, {
-						bag = bag,
-						slot = slot,
-					})
-
-					data.count = data.count + 1
-					data.value = data.value + (vendorPrice * stackCount)
-				end
-			end
-		end
-	end
-
-	return data
-end
-
---------------------------------------------------
--- Sprzedaż
---------------------------------------------------
-
-function Merchant:SellVendorItems()
-	if not self.cache then
-		return
-	end
-
-	local sold = 0
-	local earned = 0
-
-	for _, item in ipairs(self.cache.items) do
-		local link = BT:GetContainerItemLink(item.bag, item.slot)
-
-		if link then
-			local _, count = BT:GetContainerItemInfo(item.bag, item.slot)
-
-			count = count or 1
-
-			local vendorPrice = select(11, GetItemInfo(link)) or 0
-
-			earned = earned + (vendorPrice * count)
-
-			if C_Container and C_Container.UseContainerItem then
-				C_Container.UseContainerItem(item.bag, item.slot)
-			else
-				UseContainerItem(item.bag, item.slot)
-			end
-
-			sold = sold + 1
-		end
-	end
-
-	DEFAULT_CHAT_FRAME:AddMessage(
-		string.format("|cff00ffcc[BagTags]|r Sold %d vendor items for %s", sold, BT:FormatMoney(earned))
-	)
-
-	frame:Hide()
-end
-
---------------------------------------------------
 -- Przycisk
 --------------------------------------------------
 
 sellButton:SetScript("OnClick", function()
-	Merchant:SellVendorItems()
+    BT:SellVendorItems()
 end)
 
 --------------------------------------------------
@@ -167,7 +80,7 @@ eventFrame:SetScript("OnEvent", function(_, event)
 		return
 	end
 
-	Merchant.cache = Merchant:GetVendorItems()
+	Merchant.cache = BT:GetVendorItems()
 
 	if Merchant.cache.count == 0 then
 		frame:Hide()
